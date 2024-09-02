e0cac9f58e1eff093c11f3fd0d3af766c4393041
BradenEverson <bradeneverson@gmail.com>
2024-09-01 19:46:06 -0500

    Loop accelerometer reads

--- accelerometer/src/main.rs
+++ accelerometer/src/main.rs
@@ -8,17 +8,17 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     i2c.smbus_write_byte(0x6B, 0x00)?; // Write 0 to the power management register
 
-    let mut buf = [0u8; 14];
-    i2c.block_read(0x3B, &mut buf)?;
+    loop {
+        let mut buf = [0u8; 14];
+        i2c.block_read(0x3B, &mut buf)?;
 
-    let accel_x = ((buf[0] as i16) << 8) | buf[1] as i16;
-    let accel_y = ((buf[2] as i16) << 8) | buf[3] as i16;
-    let accel_z = ((buf[4] as i16) << 8) | buf[5] as i16;
+        let accel_x = ((buf[0] as i16) << 8) | buf[1] as i16;
+        let accel_y = ((buf[2] as i16) << 8) | buf[3] as i16;
+        let accel_z = ((buf[4] as i16) << 8) | buf[5] as i16;
 
-    println!("Accelerometer X: {}", accel_x);
-    println!("Accelerometer Y: {}", accel_y);
-    println!("Accelerometer Z: {}", accel_z);
-
-    Ok(())
+        println!("Accelerometer X: {}", accel_x);
+        println!("Accelerometer Y: {}", accel_y);
+        println!("Accelerometer Z: {}", accel_z);
+    }
 }
 
