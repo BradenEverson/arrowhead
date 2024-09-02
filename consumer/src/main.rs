1d5868b72545539f455409d6ac6f3eb5d5f82e80
BradenEverson <bradeneverson@gmail.com>
2024-09-02 08:05:13 -0500

    Move example to new package

---
+++ consumer/src/main.rs
@@ -0,0 +1,14 @@
+use std::error::Error;
+
+use gy521_rppal::Gy521;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let gyro = Gy521::new(1, 0x68)?;
+    gyro.wakeup()?;
+
+    loop {
+        let (pitch, poll) = gyro.read_raw_poll_pitch()?;
+
+        println!("Pitch: {:.2}\nPoll: {:.2}", pitch, poll);
+    }
+}
