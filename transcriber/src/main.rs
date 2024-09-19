9b9fe624f6111384c43869caebd933e1d7389622
BradenEverson <bradeneverson@gmail.com>
2024-09-19 07:00:03 -0500

    Create transcriber crate

---
+++ transcriber/src/main.rs
@@ -0,0 +1,14 @@
+use hc_sr04::{HcSr04, Unit};
+
+fn main() {
+    let mut ultrasonic = HcSr04::new(
+        24,          // TRIGGER -> Gpio pin 24
+        23,          // ECHO -> Gpio pin 23
+        Some(23_f32) // Ambient temperature (if `None` defaults to 20.0C)
+        ).unwrap();
+
+    match ultrasonic.measure_distance(Unit::Meters).unwrap() {
+        Some(dist) => println!("Distance: {}m", dist),
+        None => println!("Object out of range"),
+    };
+}
